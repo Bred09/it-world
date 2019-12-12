d2146f52c006898367b70c7a6983d5c759f39fbd
Bred09 <bred.king@bk.ru>
2019-12-13 01:16:39 +0500

    First commit

---
+++ js/common.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	 //$(".movLeft").addClass('animated fadeInLeft');
+	 
+	 $(".movHead").animated("fadeInDown");
+	 
+	 $(".movLeft").animated("fadeInLeft");
+	 
+	 $(".movRight").animated("fadeInRight");
+	 
+	 $(".movIn").animated("zoomIn");
+
+	 $(".photo").animated("bounceInRight");
+	
+});
+
---
+++ ru/js/common.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+
+	 //$(".movLeft").addClass('animated fadeInLeft');
+	 
+	 $(".movHead").animated("fadeInDown", "fadeOut");
+	 
+	 $(".movLeft").animated("fadeInLeft", "fadeOut");
+	 
+	 $(".movRight").animated("fadeInRight", "fadeOut");
+	 
+	 $(".movIn").animated("zoomIn", "fadeOut");
+
+	 $(".photo").animated("bounceInRight", "fadeOut");
+	
+});
+
